a5a9a1c880c2f872cfa01421cb918c5cf846d00c
Kevin Alvarez <kevinalvarezcampos@gmail.com>
2025-06-15 19:16:04 -0700

    Implemented full line buffering, however at the start of each new startup, weird implementation is occuring

--- main.c
+++ main.c
@@ -2,21 +2,23 @@
 
 
 int main() {
-    startup_banner();
+	startup_banner();
 
-    int line_num = 0;
+	int line_num = 0;
 
-    printf("USER_NAME_HEAR %d ", line_num);
-    line_num++;
+	printf("USER_NAME_HEAR %d ", line_num);
+	line_num++;
+	char buffer[1024]; 
 
-    // main loop
-    while (1) {
-        int input = getchar();
-        if (input == '\n') {
-            printf("\nUSER_NAME_HEAR %d ", line_num);
-            line_num++;
-        }
-    }
+	// main loop
+	while (1) {
+		int input = getchar();
+		if (input == '\n') {
+			while (fgets(buffer, sizeof(buffer), stdin)) {
+				printf("USER_NAME_HEAR %d ", line_num++);
+			}
+		}
+	}
 
-    return 0;
+	return 0;
 }
